658f331ffceff71ab202b2cb62fa7bf038db71aa
Kunikazu <200378857@student.georgianc.on.ca>
2018-10-08 21:06:03 -0400

    change mode to history

--- src/router.js
+++ src/router.js
@@ -6,28 +6,29 @@ import ProgrammingSkills from "./views/ProgrammingSkills";
 import Contact from "./views/Contact";
 
 Vue.use(VueRouter);
-const routes=[
-    {
-        path: '/',
-        name:"top",
-        component:Top
-    },
-    {
-        path: "/about-me",
-        name:"about-me",
-        component:AboutMe
-    },
-    {
-        path: "/programming-skills",
-        name:"programming-skills",
-        component:ProgrammingSkills
-    },
-    {
-        path: "/contact",
-        name:"contact",
-        component:Contact
-    }
+const routes = [
+  {
+    path: "/",
+    name: "top",
+    component: Top
+  },
+  {
+    path: "/about-me",
+    name: "about-me",
+    component: AboutMe
+  },
+  {
+    path: "/programming-skills",
+    name: "programming-skills",
+    component: ProgrammingSkills
+  },
+  {
+    path: "/contact",
+    name: "contact",
+    component: Contact
+  }
 ];
 export default new VueRouter({
-    routes:routes
+  mode: "history",
+  routes: routes
 });
